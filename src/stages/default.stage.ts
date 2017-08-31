642029acbf6192624d5707d0dd4b06154885878f
Jeremiah Prentice <jprentice@invalidusername.com>
2017-08-30 22:14:45 -0400

    Refactored stages

--- src/stages/default.stage.ts
+++ src/stages/default.stage.ts
@@ -2,52 +2,48 @@ import * as PIXI from 'pixi.js';
 import { Stage } from './stage';
 import { KeyboardInput } from '../inputs/keyboard.input';
 import { SpriteService } from '../services/sprite.service';
+import { Prop } from '../props/prop';
 import { Hero } from '../actors/hero.actor';
 import { Monster } from '../actors/monster.actor';
 import { Player } from '../players/player';
-import { LoopService } from '../services/loop.service';
 
 export class DefaultStage extends Stage {
 
   private _hero: Hero;
-  private _monster: Monster;
   private _player: Player;
 
   constructor() {
     super();
-    this._setupBackground();
+    this._setupMap();
+    this._setupProps();
     this._setupActors();
     this._setupPlayers();
-    LoopService.gameLoop.subscribe(() => this._update());
   }
 
-  private _setupBackground(): void {
+  private _setupMap(): void {
     this.container.addChild(SpriteService.getSprite('background.png'));
+    this._boundary = {
+      x: 0,
+      y: 0,
+      width: 256,
+      height: 256
+    };
+  }
+
+  private _setupProps(): void {
+    this._addProp(new Prop(), new PIXI.Point(100, 100));
   }
 
   private _setupActors(): void {
-    
-    this._monster = new Monster();
-    this._monster.sprite.position.set(150,150);
-    this.container.addChild(this._monster.sprite);
-
     this._hero = new Hero();
-    this._hero.sprite.position.set(128,128);
-    this.container.addChild(this._hero.sprite);
+    this._addActor(this._hero, new PIXI.Point(128, 128));
+    this._addActor(new Monster(), new PIXI.Point(150, 150));
   }
   
   private _setupPlayers(): void {
     this._player = new Player();
     this._player.input = new KeyboardInput().stream
     this._hero.setPlayer(this._player);
-  }
-
-  private _update(): void {
-    this._detectCollisions();
-  }
-
-  private _detectCollisions(): void {
-
   }
 
 }
--- src/stages/stage.ts
+++ src/stages/stage.ts
@@ -1,14 +1,51 @@
 import * as PIXI from 'pixi.js';
 import { SpriteService } from '../services/sprite.service';
+import { Prop } from '../props/prop';
+import { Actor } from '../actors/actor';
+import { LoopService } from '../services/loop.service';
+
+export interface IBoundary {
+  x: number,
+  y: number,
+  width: number,
+  height: number
+}
 
 export class Stage {
 
-  protected _container = new PIXI.Container();
+  protected _container: PIXI.Container;
+  protected _boundary: IBoundary;
+  protected _props: Prop[];
+  protected _actors: Actor[];
 
-  constructor() { }
+  constructor() {
+    this._container = new PIXI.Container();
+    this._props = [];
+    this._actors = [];
+    LoopService.gameLoop.subscribe(() => this._update());
+  }
 
   public get container(): PIXI.Container {
     return this._container;
+  }
+  
+  protected _addProp(prop: Prop, position?: PIXI.Point): void {
+    this._props.push(prop);
+    this.container.addChild(prop.sprite);
+    if (position) prop.sprite.position = position;
+  }
+  
+  protected _addActor(actor: Actor, position?: PIXI.Point): void {
+    this._actors.push(actor);
+    this.container.addChild(actor.sprite);
+    if (position) actor.sprite.position = position;
+  }
+  
+  protected _update(): void {
+    this._detectCollisions();
+  }
+
+  protected _detectCollisions(): void {
   }
 
 }
